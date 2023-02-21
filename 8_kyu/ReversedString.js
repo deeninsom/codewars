ede230db4620839dc0d26a58cbf814906cf0a041
deeninsom <syihab855@gmail.com>
2023-02-22 00:19:28 +0700

    next commit

---
+++ 8_kyu/ReversedString.js
@@ -0,0 +1,13 @@
+function solution(str){
+  let temp = ""
+  for(let i = str.length - 1; i >= 0; i--){
+    temp += str[i]
+  }
+  return temp
+}
+
+
+console.log(solution('world'), 'dlrow');
+console.log(solution('hello'), 'olleh');
+console.log(solution(''), '');
+console.log(solution('h'), 'h');
