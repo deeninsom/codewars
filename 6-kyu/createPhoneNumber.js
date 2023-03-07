d7f2af04b32dcfd543fc7cec4b1b0d235a58dda5
deeninsom <syihab855@gmail.com>
2023-03-08 01:01:03 +0700

    update doc

--- 6-kyu/createPhoneNumber.js
+++ 6-kyu/createPhoneNumber.js
@@ -1,13 +1,18 @@
 function createPhoneNumber(numbers) {
     let result = ""
     let hitung = 0;
+
+    //looping setiap numbers yang ada
     for (let i = 0; i < numbers.length; i++) {
+        // masukan numbers yagn telah di loping ke dalam temporary variable
         result += numbers[i] ;
         hitung++;
-        let next = hitung + 3
+
+        //jika sudah mendapatkan 3 angka didepan tambah string "()"
         if (hitung === 3) {
-            result = "(" + result + ") " 
+            result = `(${result}) ` 
         }
+        //setelah mendapatkan 3 value pertama, cari 3 value selanjutnya dan tambahkan stripe
         else if(hitung === 6){
             result = result + "-"
         }
