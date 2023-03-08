e37447b44ce17138150d00c43d4b50068abfb03d
deeninsom <syihab855@gmail.com>
2023-03-08 18:44:21 +0700

    update 8_kyu

---
+++ 8_kyu/exclusiveor(xor)LogicalOperator.js
@@ -0,0 +1,15 @@
+function xor(a, b) {
+    // TODO: Program Me
+    return (a == false && b == false) ? false : 
+    (a == true && b == false) ? true : 
+    (a == false && b == true) ? true : 
+    (a == true && b == true) ? false : 0
+
+
+}
+
+
+console.log(xor(false, false))
+console.log(xor(true, false))
+console.log(xor(false, true))
+console.log(xor(true, true))
